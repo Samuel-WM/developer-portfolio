73f8e9737e803df4b4205ae04f8b66a25901643f
Samuel-WM <samuelwalesmcgrath@gmail.com>
2025-07-31 12:24:10 -0400

    resume removed

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -63,7 +63,9 @@ function HeroSection() {
               <RiContactsFill size={16} />
             </button>
           </Link>
-
+         
+         
+          {/*
           <Link
             className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
             role="button"
@@ -73,6 +75,8 @@ function HeroSection() {
             <span>Get Resume</span>
             <MdDownload size={16} />
           </Link>
+          */}
+          
         </div>
       </div>
     </section>
